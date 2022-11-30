872926acb5186ea0c110a57d9b5e04d1fc48a9c5
qduy119 <87902321+qduy119@users.noreply.github.com>
2022-11-30 16:20:48 +0700

    Update ultility.py

--- function/ultility.py
+++ function/ultility.py
@@ -5,7 +5,7 @@ def getRequest(request):
     class Parse:
         def __init__(self, request):
             if request == "":
-                self.empty = True	# If there is no request
+                self.empty = True	# Nếu không có request
                 self.method = ""
                 self.path = ""
                 self.content = ""
@@ -27,6 +27,7 @@ def Response(path):
         filePath = ""
         contentType = ""
         startLine = ""
+        connection = "Connection: close"
         if(("page" + path) in listFiles):
             status = 401 if(path == "/401.html") else 200
             filePath = "page" + path
@@ -34,8 +35,7 @@ def Response(path):
             status = 404
             filePath = "page/404.html"
 
-        # self.connection = "Keep-Alive: timeout=10, max=100\r\nConnection: keep-alive"
-        # self.connection = "Connection: close"
+        # Chỉ xét những Content-Type có trong đồ án này
         if(typeOfFile in ["html", "htm"]):
             contentType = "Content-Type: text/html"
         elif(typeOfFile == "css"):
@@ -47,7 +47,7 @@ def Response(path):
         elif(typeOfFile == "ico"):
             contentType = "Content-Type: image/x-icon"
         else:
-            # Content type: text/html to load page 404 error when file does not exist
+            # Content-Type: text/html để load page 404 khi file không tồn tại
             contentType = "Content-Type: text/html"
 
         if(status == 200):
@@ -59,20 +59,20 @@ def Response(path):
 
         content = open(filePath, "rb").read()
         contentLength = "Content-Length: %d"%(len(content))
-        headerResponse = "%s\r\n%s\r\n%s\r\n\r\n"%(startLine, contentType, contentLength)
+        headerResponse = "%s\r\n%s\r\n%s\r\n%s\r\n\r\n"%(startLine, contentType, contentLength, connection)
         print("--------------------\n[HEADER RESPONSE for %s]\n%s"%(path, headerResponse))
         result = headerResponse.encode("utf-8") + content + "\r\n".encode("utf-8")
         print("--------------------\n[SERVER]\nTransfer file %s completely\n"%(filePath.split("/")[-1]))
         return result
 
-# Get list of all files in folder "Page"
+# Hàm lấy tất cả các file trong folder "Page"
 def getListOfFiles(dirName):
-    # Create a list of file and sub directories  
+    # # Tạo một danh sách chứa file và các đường dẫn con  
     listOfFile = os.listdir(dirName)
     allFiles = list()
     for entry in listOfFile:
         fullPath = dirName + '/' + entry
-        # If entry is a directory then get the list of files in this directory 
+        # Nếu entry cũng là 1 đường dẫn thì lấy danh sách các file trong đường dẫn đó
         if os.path.isdir(fullPath):
             allFiles = allFiles + getListOfFiles(fullPath)
         else:
